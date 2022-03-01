23758ea8a2f0369bbe47ee1f40676b76491c3238
Eric Hart <>
2022-03-01 20:54:58 +0800

    [Add]

---
+++ src/main/preload.js
@@ -0,0 +1,10 @@
+import { contextBridge, } from 'electron';
+import ReceiveData from '~/main/class/ReceiveData';
+import Ipc from '~/main/class/Ipc';
+
+const ipc = new Ipc();
+new ReceiveData(ipc).start();
+contextBridge.exposeInMainWorld('ipc', {
+  on: ipc.on.bind(ipc),
+  send: ipc.on.bind(ipc),
+});
